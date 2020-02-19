f401bb19555b20b3d9e6bb38dbba6571a8d425ef
Tommy Waalkes <Tommy@grandcircus.co>
2020-02-19 18:48:21 -0500

    Update Program.cs

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,7 +6,9 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            //Beavis was here
             Console.WriteLine("Hello World!");
+            //Elmos world
             Console.WriteLine("C# is fun!");
 
         }
